257867f0d7c5ff10ad0f872db3117e08b1fffa75
AlexMezen <mezentsev20001028@gmail.com>
2024-02-27 17:27:01 +0200

    lang menu

---
+++ js/lang.js
@@ -0,0 +1,98 @@
+const langArr = {
+  unit: {
+    it: "Coperture SCHILDCO",
+    de: "Wintergarten",
+  },
+  langbtn: {
+    it: "Lingua",
+    de: "Sprache",
+  },
+  navhome: {
+    it: "Home",
+    de: "Heim",
+  },
+  navprod: {
+    it: "Prodotti",
+    de: "Produkte",
+  },
+  navabout: {
+    it: "Chi Siamo",
+    de: "Über Uns Überdachungen",
+  },
+  slidetext1: {
+    it: "Design Moderni",
+    de: "Moderne Designs",
+  },
+  slidetext2: {
+    it: "Visualizza Progetto",
+    de: "Projekt anzeigen",
+  },
+  slidetext3: {
+    it: "Pulito e Minimale",
+    de: "Sauber & Minimalistisch",
+  },
+  slidetext4: {
+    it: "Personalizzare e Personalizzare",
+    de: "Personalisieren und Anpassen",
+  },
+  underslidetext: {
+    it: "Bellezza, tecnologia, eleganza",
+    de: "Schönheit, Technologie, Eleganz",
+  },
+  productsubtitle: {
+    it: "Alcuni dei nostri lavori",
+    de: "Einige unserer Arbeiten",
+  },
+  producttitle: {
+    it: "I nostri prodotti",
+    de: "Unsere Produkte",
+  },
+  imgsubtext1: {
+    it: "Coperture Telescopiche Motorizzate",
+    de: "Motorisierte Teleskopabdeckungen",
+  },
+  imgsubtext2: {
+    it: "Pergole Retrattili",
+    de: "Einziehbare Pergolen",
+  },
+  imgsubtext3: {
+    it: "Pergole Bioclimatiche",
+    de: "Bioklimatische Pergolen",
+  },
+  imgsubtext4: {
+    it: "Sfoglia tutti i prodotti",
+    de: "Alle Produkte durchsuchen",
+  },
+  subustitle: {
+    it: "Iscriviti a noi",
+    de: "Abonniere uns",
+  },
+  subustext: {
+    it: "Unisciti alla nostra newsletter per rimanere aggiornato su super sconti e offerte incredibili. Più di 300 persone si sono già iscritte.",
+    de: "Treten Sie unserem Newsletter bei, um über Super-Rabatte und erstaunliche Angebote auf dem Laufenden zu bleiben. Bereits über 300 Personen haben sich bei uns angemeldet.",
+  },
+  subusplacehold: {
+    it: "Inserisci il tuo indirizzo email",
+    de: "Geben Sie Ihre E-Mail-Adresse ein",
+  },
+  subusbtn: {
+    it: "Iscriviti",
+    de: "Abonnieren",
+  },
+  subtitleblog: {
+    it: "Leggi il nostro blog",
+    de: "Lesen Sie unseren Blog",
+  },
+  titleblog: {
+    it: "Ultimo blog",
+    de: "Neuester Blog",
+  },
+  contactus: {
+    it: "Contattaci",
+    de: "Kontaktieren Sie uns",
+  },
+  contactus: {
+    it: "Comunicate con noi",
+    de: "Kontaktieren Sie uns bitte",
+  },
+};
